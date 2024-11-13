6acf4e949a29c40a5228ecca3c8d87708ff5adee
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-11-13 10:03:15 -0300

    react charts

--- src/pages/auth/graficas/index.tsx
+++ src/pages/auth/graficas/index.tsx
@@ -1,68 +1,7 @@
-import { useMemo } from "react";
-import { AxisOptions, Chart } from "react-charts";
-
-type DailyStars = {
-  date: Date;
-  stars: number;
-};
-
-type Series = {
-  label: string;
-  data: DailyStars[];
-};
-
-const data: Series[] = [
-  {
-    label: "React Charts",
-    data: [
-      {
-        date: new Date(),
-        stars: 40,
-      },
-      // ...
-    ],
-  },
-  {
-    label: "React Query",
-    data: [
-      {
-        date: new Date(),
-        stars: 30,
-      },
-      // ...
-    ],
-  },
-];
+import { Container } from "@chakra-ui/react";
 
 function Graficas() {
-  const primaryAxis = useMemo<AxisOptions<(typeof data)[number]["data"][number]>>(
-    () => ({
-      getValue: (datum) => datum.stars,
-    }),
-    []
-  );
-
-  const secondaryAxes = useMemo<AxisOptions<(typeof data)[number]["data"][number]>[]>(
-    () => [
-      {
-        getValue: (datum) => datum.stars,
-      },
-    ],
-    []
-  );
-
-  return (
-    <div>
-      <Chart
-        options={{
-          data,
-          primaryAxis,
-          secondaryAxes,
-        }}
-      />
-      <div style={{ minHeight: "80vh" }} />
-    </div>
-  );
+  return <Container></Container>;
 }
 
 export default Graficas;
