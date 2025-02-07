00b7e82276b4737bcaf7e131b61bd83465db340d
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-02-07 16:47:46 -0300

    activar contrato

--- src/pages/auth/clientes/modal_ficha.tsx
+++ src/pages/auth/clientes/modal_ficha.tsx
@@ -1,88 +1,89 @@
 import { Button, Image, List, ListItem, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Skeleton, Stack } from "@chakra-ui/react";
 import { useClienteProvider } from "./useclienteprovider";
 import moment from "moment";
-import {useState,useEffect,useCallback} from 'react'
+import { useState, useEffect, useCallback } from "react";
 import { APICALLER } from "../../../services/api";
 import userDataHook from "../../../store/user_data_store";
 import { env } from "../../../config/env";
 
 function ModalFicha() {
+  const { dataUser } = userDataHook();
+  const [loading, setLoading] = useState(true);
+  const { modals, setModals, form } = useClienteProvider();
+  const initialDatos = { foto_ci_frente: "" };
+  const [datos, setDatos] = useState(initialDatos);
+  const onClose = () => {
+    setModals({ ...modals, ficha: false });
+    setDatos(initialDatos);
+  };
 
-    const {dataUser} = userDataHook()
-    const [loading,setLoading] = useState(true)
-    const {modals,setModals,form} = useClienteProvider()
-    const initialDatos = {foto_ci_frente:''} 
-    const [datos,setDatos] = useState(initialDatos)
-    const onClose = ()=>{ setModals({...modals,ficha:false}); setDatos(initialDatos) }
-
-
-    const getInfo = useCallback(async()=>{
-      if(modals.ficha){
-        const res = await APICALLER.fichaCliente({token:dataUser.token,id:form.id})
-        if(res.success){
-          const f = res.results;          
-          setDatos({
-            foto_ci_frente:f.foto_ci_frente
-          })
-        }
+  const getInfo = useCallback(async () => {
+    if (modals.ficha) {
+      const res = await APICALLER.fichaCliente({ token: dataUser.token, id: form.id });
+      if (res.success) {
+        const f = res.results;
+        setDatos({
+          foto_ci_frente: f.foto_ci_frente,
+        });
       }
-      setLoading(false)
-    },[dataUser,form,modals.ficha])
+    }
+    setLoading(false);
+  }, [dataUser, form, modals.ficha]);
 
-
-    
-    useEffect(() => {
-      const ca = new AbortController(); let isA = true;
-      isA && getInfo();
-      return () => {isA = false;ca.abort();};
+  useEffect(() => {
+    const ca = new AbortController();
+    let isA = true;
+    isA && getInfo();
+    return () => {
+      isA = false;
+      ca.abort();
+    };
   }, [getInfo]);
 
-    return ( <Modal isOpen={modals.ficha} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Ficha de cliente</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            {
-              loading ?
-              <Stack direction='column'>
-                <Skeleton h='10px' />
-                <Skeleton h='10px' />
-                <Skeleton h='10px' />
-              </Stack>
-              :
-              <Stack direction='column'>
-                <List>
-                  <ListItem>
-                    <u>Nombre:</u> {form.name}
-                  </ListItem>
-                  <ListItem>
-                    <u>Cedula:</u> {form.cedula}
-                  </ListItem>
-                  <ListItem>
-                    <u>Email:</u> {form.email}
-                  </ListItem>
-                  <ListItem>
-                    <u>Telef:</u> {form.celular}
-                  </ListItem>
-                  <ListItem>
-                    <u>Fecha de registro:</u> {moment.utc(form.created_at).format('HH:mm DD-MMM-YYYY')} 
-                  </ListItem>
-                </List>
-                <Image
-                    boxSize='360px'
-                    objectFit='cover'
-                    src={env.PUBLIC_PATH+`/${datos?.foto_ci_frente}`}
-                    alt='Frontal'
-                  />
-              </Stack>
-            }
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={onClose} variant='outline'>Cerrar</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>  );
+  return (
+    <Modal isOpen={modals.ficha} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Ficha de cliente</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          {loading ? (
+            <Stack direction="column">
+              <Skeleton h="10px" />
+              <Skeleton h="10px" />
+              <Skeleton h="10px" />
+            </Stack>
+          ) : (
+            <Stack direction="column">
+              <List>
+                <ListItem>
+                  <u>Nombre:</u> {form.name}
+                </ListItem>
+                <ListItem>
+                  <u>Cedula:</u> {form.cedula}
+                </ListItem>
+                <ListItem>
+                  <u>Email:</u> {form.email}
+                </ListItem>
+                <ListItem>
+                  <u>Telef:</u> {form.celular}
+                </ListItem>
+                <ListItem>
+                  <u>Fecha de registro:</u> {moment.utc(form.created_at).format("HH:mm DD-MMM-YYYY")}
+                </ListItem>
+              </List>
+              <Image boxSize="360px" objectFit="cover" src={env.PUBLIC_PATH + `${datos?.foto_ci_frente}`} alt="Frontal" />
+            </Stack>
+          )}
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={onClose} variant="outline">
+            Cerrar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
 
 export default ModalFicha;
--- src/pages/auth/contratos/index.tsx
+++ src/pages/auth/contratos/index.tsx
@@ -1,13 +1,32 @@
+import { env } from "@/config/env";
 import { APICALLER } from "@/services/api";
 import userDataHook from "@/store/user_data_store";
-import { Box, Container, Input } from "@chakra-ui/react";
-import { useRef, useState } from "react";
+import { Box, Container, GridItem, Grid, Input, Image, Button } from "@chakra-ui/react";
+import { Fragment, useRef, useState } from "react";
 import React from "react";
+
+type ContratosDataType = {
+  estrDescripcion: string;
+  micoCantImpresiones: number;
+  micoCodigo: number;
+  micoEstado: string;
+  usuarioImpresor: string;
+  micoValorLineaCredito: number;
+};
+type ClienteDataType = {
+  foto_ci_frente: string;
+  id: number;
+};
+type resultsType = {
+  cliente: ClienteDataType;
+  contratos: ContratosDataType[];
+};
 
 function ContratosPage() {
   const { dataUser } = userDataHook();
   const [loading, setLoading] = useState(false);
   const buscarRef = useRef(null);
+  const [results, setResults] = useState<resultsType | null>(null);
 
   const buscarEnter = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
@@ -17,10 +36,19 @@ function ContratosPage() {
         setLoading(true);
         const { results, success } = await APICALLER.consultaEstadoContrato(buscar, dataUser.token);
         setLoading(false);
+
+        if (!success) {
+          setResults(null);
+          return;
+        }
+
         if (success) {
           document.getElementById("_buscar")?.focus;
+          setResults({
+            cliente: results.cliente,
+            contratos: results.contratos,
+          });
         }
-        console.log(results);
       }
     }
   };
@@ -28,10 +56,35 @@ function ContratosPage() {
   return (
     <Container>
       <h3>Consultas de contrato blupy</h3>
-
       <Box>
-        <Input disabled={loading} id="_buscar" ref={buscarRef} placeholder="Busca y presiona ENTER..." onKeyUp={buscarEnter} />
+        <Input disabled={loading} id="_buscar" ref={buscarRef} placeholder="Ingresa cedula y presiona ENTER..." onKeyUp={buscarEnter} />
       </Box>
+      {results && results.cliente && (
+        <Box>
+          <Grid templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(2, 1fr)", "repeat(2, 1fr)"]} gap={4}>
+            <GridItem>
+              <h4>Nombre:</h4>
+              <p>{results.cliente.id}</p>
+              <h4>Cedula:</h4>
+              <Image src={env.PUBLIC_PATH + results.cliente.foto_ci_frente} />
+            </GridItem>
+            <GridItem>
+              {results.contratos.map((e, i) => (
+                <Fragment key={i}>
+                  <h4>Contrato</h4>
+                  <p>Descripcion: {e.estrDescripcion}</p>
+                  <p>Cantidad de impresiones: {e.micoCantImpresiones}</p>
+                  <p>Codigo de contrato: {e.micoCodigo}</p>
+                  <p>Estado: {e.micoEstado}</p>
+                  <p>Impresor: {e.usuarioImpresor}</p>
+                  <p>Valor linea de credito: {e.micoValorLineaCredito}</p>
+                  <Button mt={3}>activar contrato</Button>
+                </Fragment>
+              ))}
+            </GridItem>
+          </Grid>
+        </Box>
+      )}
     </Container>
   );
 }
