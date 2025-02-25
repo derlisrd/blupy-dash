93392bed2fae3c4e3f4583202d54b0f12ce0095c
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2025-02-25 03:37:13 -0300

    z

--- src/app/auth/clientes/index.tsx
+++ src/app/auth/clientes/index.tsx
@@ -1,27 +1,33 @@
 import useClientes from "@/core/hooks/clientes/useClientes";
-import {
-  Box,
-  Container,
-  LinearProgress,
-  Paper,
-  Stack,
-  Grid2 as Grid,
-  TextField,
-  InputAdornment,
-  Icon,
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  Checkbox,
-  TableBody,
-  IconButton,
-  TableFooter,
-} from "@mui/material";
+import { Box, Container, LinearProgress, Paper, Stack, Grid2 as Grid, TextField, InputAdornment, Icon, TableContainer } from "@mui/material";
+import AutoSizer from "react-virtualized-auto-sizer";
+import { Column, Table, TableCellProps, TableHeaderProps } from "react-virtualized";
+import MuiTableCell from "@mui/material/TableCell";
+
+const TableCell = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <MuiTableCell
+      sx={{
+        flex: 1,
+        display: "flex",
+        alignItems: "center",
+        boxSizing: "border-box",
+        cursor: "pointer",
+        height: 48,
+      }}
+      variant="head"
+    >
+      {children}
+    </MuiTableCell>
+  );
+};
 
 function ClientesList() {
   const { lista, isLoading } = useClientes();
+
+  const headerRenderer = ({ label }: TableHeaderProps) => <TableCell>{label}</TableCell>;
+  const cellRenderer = ({ cellData }: TableCellProps) => <TableCell>{cellData}</TableCell>;
+
   return (
     <Container sx={{ paddingBottom: 4 }}>
       <Stack direction={{ xs: "row" }} justifyContent="space-between" alignItems="center" padding={2}>
@@ -50,42 +56,28 @@ function ClientesList() {
             </Grid>
             <Grid size={{ xs: 12, md: 4 }}></Grid>
           </Grid>
-          <TableContainer component={Paper} sx={{ borderRadius: 0, border: 0, boxShadow: 0 }}>
-            <Table stickyHeader size="small">
-              <TableHead>
-                <TableRow>
-                  <TableCell>
-                    <Checkbox />
-                  </TableCell>
-                  <TableCell>Doc.</TableCell>
-                  <TableCell>Nombre</TableCell>
-                  <TableCell>Fecha</TableCell>
-                  <TableCell>
-                    <span></span>
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {lista &&
-                  lista.map((item, index) => (
-                    <TableRow key={index} hover>
-                      <TableCell>
-                        <Checkbox />
-                      </TableCell>
-                      <TableCell>{item.cedula}</TableCell>
-                      <TableCell>{item.name}</TableCell>
-                      <TableCell>{item.created_at}</TableCell>
-                      <TableCell>
-                        <IconButton>
-                          <Icon>more_vert</Icon>
-                        </IconButton>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-              </TableBody>
-
-              <TableFooter></TableFooter>
-            </Table>
+          <TableContainer component={Paper} sx={{ borderRadius: 0, border: 0, boxShadow: 0, minHeight: `calc(100% - 280px)` }}>
+            {lista && (
+              <AutoSizer>
+                {({ height, width }) => (
+                  <Table
+                    height={height}
+                    width={width}
+                    rowHeight={48!}
+                    headerHeight={48!}
+                    rowStyle={{ display: "flex", alignItems: "center" }}
+                    rowCount={lista.length}
+                    rowGetter={({ index }) => lista[index]}
+                  >
+                    <Column headerRenderer={headerRenderer} dataKey="id" label="#" width={80} cellRenderer={cellRenderer} />
+                    <Column headerRenderer={headerRenderer} dataKey="cedula" label="Cedula" width={80} cellRenderer={cellRenderer} />
+                    <Column headerRenderer={headerRenderer} cellRenderer={cellRenderer} dataKey="name" label="Nombre" width={250} />
+                    <Column headerRenderer={headerRenderer} cellRenderer={cellRenderer} dataKey="vendedor_id" label="vendedor" width={90} />
+                    <Column headerRenderer={headerRenderer} cellRenderer={cellRenderer} dataKey="created_at" label="fecha" width={128} />
+                  </Table>
+                )}
+              </AutoSizer>
+            )}
           </TableContainer>
         </Box>
       )}
